2fee36a91dd0bb0f2b6b3f083109c739916becf6
Keith Farrugia <keithfarrugia04@gmail.com>
2026-02-06 01:43:20 +0100

    clean

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -32,16 +32,9 @@ def get_by_id(student_id=None):
     if not student:
         return 'not found', 404
 
-    # Ensure the JSON response matches the expected schema
-    student_response = {
-        "student_id": student["student_id"],
-        "first_name": student["first_name"],
-        "last_name": student["last_name"],
-        "grade_records": student.get("grade_records", [])  # empty list if none
-    }
 
-    print(student_response)
-    return student_response, 200
+    print(student)
+    return student, 200
 
 
 def delete(student_id=None):
